dced30c098603b996cc08b898974d0f4e1a9bbad
befint1989 <lizongyu@lizongyudemacbook-pro.local>
2016-09-08 00:59:36 +0800

    練習scrollview從子視窗設定高度

--- uiscrollview/ViewController.swift
+++ uiscrollview/ViewController.swift
@@ -9,11 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var addSubViewHight: NSLayoutConstraint!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    @IBAction func restoreHeight(sender: AnyObject) {
+        addSubViewHight.constant = 0
+        
+    }
+
+    
+    @IBAction func addsubview(sender: AnyObject) {
+        
+        addSubViewHight.constant = 100
+   
+    }
+
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
